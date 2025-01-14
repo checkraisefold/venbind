9dba4ef9c97f21f3e5bc602d4ccf1437247e2680
checkraisefold <checkraisefold@crf.gay>
2025-01-13 20:29:07 -0800

    nice.

--- devenv.nix
+++ devenv.nix
@@ -27,7 +27,7 @@
       targets = [
         # "aarch64-unknown-linux-gnu"
         "x86_64-unknown-linux-gnu"
-        "x86_64-pc-windows-gnu"
+        "x86_64-pc-windows-msvc"
         # "aarch64-pc-windows-msvc"
         # "aarch64-pc-windows-msvc"
         # "x86_64-apple-darwin"
